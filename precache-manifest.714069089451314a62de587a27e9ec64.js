bd440e4c5588e610903a99de2c5c7f84b087657c
Anurag Gupta <anuraggupta5086@gmail.com>
2020-09-04 11:13:43 +0400

    Updates

---
+++ precache-manifest.714069089451314a62de587a27e9ec64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a47ab5f8a6b17e2602e38f6b7074ba",
+    "url": "/instagram-clone-react/index.html"
+  },
+  {
+    "revision": "20150d4b72d0953fad42",
+    "url": "/instagram-clone-react/static/css/main.817c6da6.chunk.css"
+  },
+  {
+    "revision": "f1bc576a7d75d1ad97da",
+    "url": "/instagram-clone-react/static/js/2.a0911752.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/instagram-clone-react/static/js/2.a0911752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20150d4b72d0953fad42",
+    "url": "/instagram-clone-react/static/js/main.987f87fb.chunk.js"
+  },
+  {
+    "revision": "497fc0ea82d334bee223",
+    "url": "/instagram-clone-react/static/js/runtime-main.6e3921e1.js"
+  }
+]);
